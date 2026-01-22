03764374b55fe05973c9118841e85c5a24a3df42
gyu-young-park <gyoue200125@gmail.com>
2026-01-22 22:47:55 +0900

    feat: add file-telemetry-infra project

---
+++ file-telemetry-infra/src/test/java/io/github/observability/filetelemetryinfra/FileTelemetryInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.observability.filetelemetryinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTelemetryInfraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
